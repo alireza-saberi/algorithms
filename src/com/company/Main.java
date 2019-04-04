669e033e3271814b7a93aa6349c872770be99f82
Alan Saberi <alan.saberi@nuance.com>
2019-04-04 18:48:08 -0400

    [#3] feat: adding selection sort 	 fix: some bug fixes for bubble sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // testing bubble sorter
         Sorter st = new Sorter();
         int[] intArray = st.sortBubble();
 
+        System.out.println("Result of bubble sort");
+        System.out.println("=====================");
         for (int i = 0; i < intArray.length; i++){
             System.out.println(intArray[i]);
         }
 
 
+        // testing selection sorter
+        Sorter st2 = new Sorter();
+        int[] intArray2 = st2.sortSelection();
 
+        System.out.println("Result of bubble sort");
+        System.out.println("=====================");
+        for (int i = 0; i < intArray2.length; i++){
+            System.out.println(intArray2[i]);
+        }
 
     }
 }
--- src/com/company/Sorter.java
+++ src/com/company/Sorter.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Sorter {
-    private int[] intArray = {20, 19, 18, 17, 16, 15,14}; // short form is allowed only at variable initialization
+    private int[] intArray = {10, 4, 5, 7, 16, 30, -10}; // short form is allowed only at variable initialization
 
 
     public Sorter() {
@@ -21,18 +21,38 @@ public class Sorter {
 
     //    one of the  least efficient algorithms
     //    order of it is quadratic
-    //    this is in-place algirithm
+    //    this is in-place algorithm
     //     this is a stable algorithm
     public int[] sortBubble(){
-        for(int lastUnsortedIndex = intArray.length; lastUnsortedIndex > 0; lastUnsortedIndex--)
-            for (int i = 0; i > lastUnsortedIndex; i++) {
-                if (intArray[i] > intArray[i++]) {
-                    swap(intArray, i, i++);
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+//                System.out.println(i);
+                if (intArray[i] > intArray[i+1]) {
+                    swap(intArray, i, i+1);
                 }
             }
+        }
         return intArray;
     }
-    
+
+    // one of the least efficient algorithms
+    // Order of it is quadratic but is not swap
+    // this is in place algorithm
+    // This is an unstable algorithm
+    public int[] sortSelection(){
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            int maxIndex = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++){
+                if(intArray[i] > intArray[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+            swap(intArray, maxIndex, lastUnsortedIndex);
+        }
+        return intArray;
+    }
 
     public static void swap(int[] array, int i, int j){
         if ( i == j ) return;
