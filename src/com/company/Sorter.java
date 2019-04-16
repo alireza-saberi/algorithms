f9e33cc1403cd36b568511689ea499e2dc9d1ec1
Alan Saberi <saberi.ar.eng@gmail.com>
2019-04-16 08:33:31 -0400

    [#4] feat: adding merge sort based on Laakmann McDowell on youtube

--- src/com/company/Sorter.java
+++ src/com/company/Sorter.java
@@ -86,4 +86,57 @@ public class Sorter {
         array[j] = temp;
 
     }
+
+    // Merge sort:  quiet efficient algorithm
+    // recursive and order is n log n
+    // this is NOT in place algorithm
+    // this is a stable algorithm
+    /*
+        the idea is like
+        mergesort(array) {
+            mergesort(array's left half)
+            mergesort(array's right side)
+            merge left & right half in sorted order
+        }
+        taken from Tutorial with Gayle Laakmann McDowell on youtube (https://www.youtube.com/watch?v=KF2j-9iSf4Q)
+     */
+    public static  void mergeSort(int[] array){
+        mergeSort(array, new int[array.length -1], 0, array.length -1);
+    }
+
+//    our recurs
+    public static void mergeSort(int[] array, int[] temp, int leftStart, int rightEnd){
+        if (leftStart >= rightEnd){
+            return;
+        }
+        int middle = (leftStart + rightEnd) / 2;
+        mergeSort(array, temp, leftStart, middle);
+        mergeSort(array, temp,middle + 1, rightEnd);
+        mergeHalves(array, temp,leftStart, rightEnd);
+    }
+
+    public static void mergeHalves(int[] array, int[] temp, int leftStart, int rightEnd ){
+        int leftEnd = (leftStart + rightEnd) / 2;
+        int rightStart = leftEnd + 1;
+        int size = leftEnd - rightStart + 1;
+
+        int left = leftStart;
+        int right = rightStart;
+        int index = leftStart;
+
+        while( left <= leftEnd && right <= rightEnd){
+            if( array[left] <= array[right]){
+                temp[index] = array[left];
+                left++;
+            } else{
+                temp[index] = array[right];
+                right++;
+            }
+            index++;
+            System.arraycopy(array, left, temp, index, leftEnd - left + 1);
+            System.arraycopy(array, right,temp, index, rightEnd - right +1);
+            System.arraycopy(temp, leftStart, array, leftStart, size);
+        }
+    }
+
 }
